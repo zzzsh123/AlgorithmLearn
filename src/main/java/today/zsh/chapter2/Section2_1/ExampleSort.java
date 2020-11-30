99fba1b274d9ebf37688e732efe70f61646d8fec
zzzsh123 <57971494+zzzsh123@users.noreply.github.com>
2020-11-30 20:38:21 +0800

    ExampleSort realize and time complexity level image

--- src/main/java/today/zsh/chapter2/Section2_1/ExampleSort.java
+++ src/main/java/today/zsh/chapter2/Section2_1/ExampleSort.java
@@ -5,22 +5,22 @@ package today.zsh.chapter2.Section2_1;
  * date: 11/27/2020 3:56 PM
  * description: A template for the sorting algorithm class  排序算法类的模板
  **/
-public class Example {
+public class ExampleSort {
 
-    private static boolean less(Comparable v, Comparable w) {
+    static boolean less(Comparable v, Comparable w) {
         return v.compareTo(w) < 0;
     }
 
-    private static void exCh(Comparable[] a,int i,int k) {
+    static void exCh(Comparable[] a,int i,int k) {
         Comparable temp = a[i]; a[i] = a[k]; a[k] = temp;
     }
 
-    private static void show(Comparable[] a) {
+    static void show(Comparable[] a) {
         for (Comparable c : a) System.out.print(c);
         System.out.println();
     }
 
-    private static boolean isSorted(Comparable[] a) {
+    static boolean isSorted(Comparable[] a) {
         for (int i = 1;i < a.length;i++) {
             if (less(a[i], a[i-1]))
                 return false;
